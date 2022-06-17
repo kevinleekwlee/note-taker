002d397415c8c35cd7acae14ff06b31ba359cdd2
Kevin Lee <kevin.lee.kwlee@gmail.com>
2022-06-17 19:51:24 -0400

    Added GET and PUT and dependencies

---
+++ server.js
@@ -0,0 +1,41 @@
+// Adding the apps dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+
+// Asynchronous Processes
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// Building out server
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static Middleware
+app.use(express.static("./develop/public"));
+
+// API route GET request
+app.get("/api/notes", function(req, res) {
+    readFileAsync("./develop/db/db.json","utf8").then(function(data){
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+    })
+});
+
+// API route POST request
+app.post("/api/notes", function(req, res) {
+    const note = req.body;
+    readFileAsync("./develop/db/db.json","utf8").then(function(data){
+        const notes = [].concat(JSON.parse(data));
+        note.id = notes.length + 1
+        notes.push(note);
+        return notes
+    }).then(function(notes) {
+        writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+        res.json(note);
+    })
+});
